343ddd261b7f2a19bd2107785304b96b301ebe86
Laura Sans <56506836+laurasansc@users.noreply.github.com>
2021-03-24 13:48:09 +0100

    update coda laura

--- scripts/bulk_analysis_coda.R
+++ scripts/bulk_analysis_coda.R
@@ -1,15 +1,14 @@
 # -------------------- Load the libraries------------------------------------
 library("tidyverse")
-library("ALDEx2")
+
 # -------------------- Read the data ----------------------------------------
 raw_bulk_counts <- read.csv("data/bulk/counts_bulk.tsv", sep = "\t")
 metadata <- read.csv("data/bulk/metadata_bulk.tsv", sep = "\t")
 # ---------------- Wrangle bulk counts --------------------------------------
-bulk_counts <- raw_bulk_counts %>% subset(select = -Gene.Name)
-bulk_counts_edgeR <- raw_bulk_counts %>%
+bulk_counts <- raw_bulk_counts %>%
   subset(select = -Gene.Name) %>%
   column_to_rownames(var = "Gene.ID")
-# -------------------- Clean metadata ---------------------------------------
+# -------------------- Clean metadata ------------------q---------------------
 # remove columns with all NA
 metadata <- metadata %>% select_if(~ !all(is.na(.)))
 # Rename columns, remove "Sample.Characteristic" and "." at the end of column name
@@ -18,11 +17,15 @@ names(metadata) <- sub(".$", "", names(metadata))
 # Drop unused columns
 # Age into only number
 metadata <- metadata %>%
-  rename(c("Ru" = "run", "sampling.site" = "sample_type")) %>%
+  rename(c("run" = "Ru", "sample_type" = "sampling.site")) %>%
   select(-contains("Ontology.Term")) %>%
   separate(age, c("age", "drop")) %>%
   subset(select = -drop)
 # ----------------------------------------------------------------------------
 # ----------------- ALDEx2 package -------------------------------------------
 # ----------------------------------------------------------------------------
+library("ALDEx2")
+conds <- metadata$sample_type
+x.all <- aldex(bulk_counts, conds, mc.samples=16, test="t", effect=TRUE,
+               include.sample.summary=FALSE, denom="all", verbose=FALSE)
 
