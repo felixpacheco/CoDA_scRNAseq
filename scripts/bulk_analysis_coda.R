dd6bc7410f8657292957443258df391a53ad8f90
Laura Sans <56506836+laurasansc@users.noreply.github.com>
2021-05-05 15:26:12 +0200

    progress

--- scripts/bulk_analysis_coda.R
+++ scripts/bulk_analysis_coda.R
@@ -3,11 +3,17 @@ library("tidyverse")
 
 # -------------------- Read the data ----------------------------------------
 raw_bulk_counts <- read.csv("data/bulk/counts_bulk_patient.tsv", sep = "\t")
-metadata <- read.csv("data/bulk/metadata_bulk.tsv", sep = "\t")
+xtable(raw_bulk_counts[c(1:5), c(1:4, 39)])
+metadata_raw <- read.csv("data/bulk/metadata_bulk.tsv", sep = "\t")
 # ---------------- Wrangle bulk counts --------------------------------------
-bulk_counts <- raw_bulk_counts %>%
-  column_to_rownames(var = "Ensembl_gene_id")
-# -------------------- Clean metadata ------------------q---------------------
+bulk_counts <- raw_bulk_counts
+bulk_counts[63152, "Ensembl_gene_id"] <- "TC_"
+bulk_counts <- bulk_counts[-c(63152), ]
+bulk_counts_clean <- bulk_counts[apply(bulk_counts[, -1], 1, function(x) !all(x == 0)), ]
+
+# Import head of raw bulk counts to latex table
+xtable(bulk_counts_clean[c(1:5), c(1:4, 39)])
+
 # -------------------- Clean metadata ---------------------------------------
 # remove columns with all NA
 metadata <- metadata_raw %>% select_if(~ !all(is.na(.)))
@@ -20,12 +26,18 @@ metadata <- metadata %>%
   rename("sample_type" = "sampling.site") %>%
   select(-contains("Ontology.Term")) %>%
   separate(age, c("age", "drop")) %>%
-  subset(select = -c(Ru,drop, organism, Factor.Value.sampling.site, Analyse))
+  subset(select = -c(Ru, drop, organism, Factor.Value.sampling.site, Analyse))
 # Now get only metadata per individual not per run
-metadata <- metadata %>% mutate(sample = ifelse(sample_type=='tumor tissue', paste(individual,"T", sep=""), paste(individual,"N", sep="")))
+metadata <- metadata %>% mutate(sample = ifelse(sample_type == "tumor tissue", paste(individual, "T", sep = ""), paste(individual, "N", sep = "")))
 
-metadata_counts <- metadata %>% group_by(sample) %>% 
-  distinct
+metadata_counts <- metadata %>%
+  group_by(sample) %>%
+  distinct()
+
+xtable(metadata_counts[c(1:5),])
+# Wrangle a bit more
+row.names(bulk_counts_clean) <- bulk_counts_clean$Ensembl_gene_id
+bulk_counts_clean <- bulk_counts_clean[, -1]
 
 # ----------------- ALDEx2 package -------------------------------------------
 # ----------------------------------------------------------------------------
@@ -34,7 +46,3 @@ library("ALDEx2")
 conds <- metadata_counts$sample_type
 x.all <- aldex(bulk_counts, conds, mc.samples=128, test="t", effect=TRUE,
                include.sample.summary=FALSE, denom="all", verbose=FALSE)
-
-# MC 128 because the t-test will be accurate
-
-
