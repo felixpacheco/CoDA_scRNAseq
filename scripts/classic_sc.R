814ac891400f861733df6b3eb28a3086ef141323
Laura Sans <56506836+laurasansc@users.noreply.github.com>
2021-05-30 22:25:37 +0200

    classic and coda

--- scripts/classic_sc.R
+++ scripts/classic_sc.R
@@ -17,16 +17,19 @@ sc_counts <- sc_counts[apply(sc_counts[, -1], 1, function(x) !all(x == 0)), ]
 
 # get ensmbl gene ids
 library(biomaRt)
-library("XML")
 
-ensembl <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
+#ensembl <- useMart("ensembl", host="uswest.ensembl.org", dataset="mmusculus_gene_ensembl", ensemblRedirect = FALSE)
 genes_names <- sc_counts$external_gene_name
 
-IDs <- getBM(attributes=c('ensembl_gene_id',
-                          'external_gene_name'),
-             filters = 'mgi_symbol',
-             values = genes_names,
-             mart = ensembl)
+#IDs <- getBM(attributes=c('ensembl_gene_id',
+#                          'external_gene_name'),
+#             filters = 'mgi_symbol',
+#             values = genes_names,
+#             mart = ensembl)
+IDs <- read_csv("data/martquery_0530110047_39.txt")
+names(IDs)[names(IDs) == 'Gene stable ID'] <- 'ensembl_gene_id'
+names(IDs)[names(IDs) == 'Gene name'] <- 'external_gene_name'
+
 
 # get annotated genes
 annotated_genes_counts <- merge(IDs,sc_counts,by="external_gene_name")
@@ -36,58 +39,59 @@ genes <- row.names(annotated_genes_counts)
 
 # ----------------- SingleCellExperiment -------------------------------------
 # Now we need to read the SingleCell experiment
-library("SingleCellExperiment")
+require("SingleCellExperiment")
 require("scran")
-library(AnnotationHub)
-library(org.Hs.eg.db)
-library(AnnotationDbi)
-library(scater)
-library(BiocSingular)
-library("scDblFinder")
+# require(AnnotationHub)
+# require(org.Hs.eg.db)
+# require(AnnotationDbi)
+# require(scater)
+# require(BiocSingular)
+# require("scDblFinder")
+# 
 ### Make single cell experiment
 sce <- SingleCellExperiment(assays = list(counts = annotated_genes_counts), 
                             colData = metadata)
-
-# Pre doublet detection
-#--- gene-annotation ---#
-#rownames(sce) <- uniquifyFeatureNames(
-# rowData(sce)$gene, rowData(sce)$Symbol)
-
-ens.mm.v93 <- AnnotationHub()[["AH64461"]]
-
-rowData(sce)$ensembl <- genes
-rowData(sce)$SEQNAME <- mapIds(ens.mm.v93, keys=rowData(sce)$ensembl,
-                               keytype="GENEID", column="SEQNAME")
-
-#--- quality-control ---#
-is.mito <- rowData(sce)$SEQNAME == "MT"
-stats <- perCellQCMetrics(sce, subsets=list(Mito=which(is.mito)))
-qc <- quickPerCellQC(stats, percent_subsets="subsets_Mito_percent")
-sce <- sce[,!qc$discard]
-
-#--- normalization ---#
-set.seed(101000110)
-clusters <- quickCluster(sce)
-sce <- computeSumFactors(sce, clusters=clusters)
-sce <- logNormCounts(sce)
-
-#--- variance-modelling ---#
-set.seed(00010101)
-dec <- modelGeneVarByPoisson(sce)
-top <- getTopHVGs(dec, prop=0.1)
-
-#--- dimensionality-reduction ---#
-set.seed(101010011)
-sce <- denoisePCA(sce, technical=dec, subset.row=top)
-sce <- runTSNE(sce, dimred="PCA")
-
-#--- clustering ---#
-snn.gr <- buildSNNGraph(sce, use.dimred="PCA", k=25)
-colLabels(sce) <- factor(igraph::cluster_walktrap(snn.gr)$membership)
-
-# Doublet detection
-dbl.out <- findDoubletClusters(sce)
-dbl.out
+# 
+# # Pre doublet detection
+# #--- gene-annotation ---#
+# #rownames(sce) <- uniquifyFeatureNames(
+# # rowData(sce)$gene, rowData(sce)$Symbol)
+# 
+# ens.mm.v93 <- AnnotationHub()[["AH64461"]]
+# 
+# rowData(sce)$ensembl <- genes
+# rowData(sce)$SEQNAME <- mapIds(ens.mm.v93, keys=rowData(sce)$ensembl,
+#                                keytype="GENEID", column="SEQNAME")
+# 
+# #--- quality-control ---#
+# is.mito <- rowData(sce)$SEQNAME == "MT"
+# stats <- perCellQCMetrics(sce, subsets=list(Mito=which(is.mito)))
+# qc <- quickPerCellQC(stats, percent_subsets="subsets_Mito_percent")
+# sce <- sce[,!qc$discard]
+# 
+# #--- normalization ---#
+# set.seed(101000110)
+# clusters <- quickCluster(sce)
+# sce <- computeSumFactors(sce, clusters=clusters)
+# sce <- logNormCounts(sce)
+# 
+# #--- variance-modelling ---#
+# set.seed(00010101)
+# dec <- modelGeneVarByPoisson(sce)
+# top <- getTopHVGs(dec, prop=0.1)
+# 
+# #--- dimensionality-reduction ---#
+# set.seed(101010011)
+# sce <- denoisePCA(sce, technical=dec, subset.row=top)
+# sce <- runTSNE(sce, dimred="PCA")
+# 
+# #--- clustering ---#
+# snn.gr <- buildSNNGraph(sce, use.dimred="PCA", k=25)
+# colLabels(sce) <- factor(igraph::cluster_walktrap(snn.gr)$membership)
+# 
+# # Doublet detection
+# dbl.out <- findDoubletClusters(sce)
+# dbl.out
 
 # ----------------------------------------------------------------------------
 # ----------------- EdgeR package --------------------------------------------
@@ -149,3 +153,30 @@ head(dge_dds, 10)
 write.csv(dge_dds,  gzfile("SC_deseq2_DE.csv.gz"), row.names = TRUE)
 # Write to file
 write.csv(res_edgeR, gzfile("SC_edgeR_DE.csv.gz"), row.names = TRUE)
+
+
+# ----------------------------------------------------------------------------
+# ----------------- ALDEx2 package -------------------------------------------
+# ----------------------------------------------------------------------------
+# no use of Single Cell Experiment
+# the BioCManager packages need to be loaded always after the data wrangling because they interfere
+library("ALDEx2")
+conds <- metadata$cell_ontology_class
+x.all <- aldex(annotated_genes_counts, conds, mc.samples=128, test="t", effect=TRUE,include.sample.summary=FALSE, denom="all", verbose=FALSE)
+
+# Write to file
+write.csv(x.all, "aldex2_DE.csv", row.names = TRUE)
+
+# clr
+x <- aldex.clr(annotated_genes_counts, conds, mc.samples=128, denom="all", verbose=F)
+
+x_df <- data.frame(x@analysisData)
+
+# now reduce the columns to one
+library(stringr)
+nm1 <- str_replace(names(x_df),"\\..*", "")
+x.new <- data.frame
+x_df[paste0(unique(nm1), "_mean")] <- sapply(split.default(x_df, nm1), rowMeans)
+x.new <- x_df[grep("_mean", names(x_df))] 
+write.csv(x.new, "aldex2_clr.csv", row.names = TRUE)
+
