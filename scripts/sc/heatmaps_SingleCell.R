4dc9e20fb877ac13999aa68352715ba21b1580ef
Laura Sans <56506836+laurasansc@users.noreply.github.com>
2021-05-31 23:22:50 +0200

    heatmaps

--- scripts/sc/heatmaps_SingleCell.R
+++ scripts/sc/heatmaps_SingleCell.R
@@ -5,29 +5,53 @@ library("xtable")
 library("viridis")
 library("hrbrthemes")
 # -------------------- Read the data ----------------------------------------
-sc_counts <- read_csv("data/singlecell/counts_sc.csv.gz")
-metadata<- read_csv("data/singlecell/metadata_sc.csv.gz")
-# ---------------- Wrangle counts & metadata --------------------------------
+sc_counts <- read_csv("/home/projects/cge/people/laucom/CoDA_scRNAseq/data/singlecell/counts_sc.csv.gz")
+metadata <- read_csv("/home/projects/cge/people/laucom/CoDA_scRNAseq/data/singlecell/metadata_sc.csv.gz")
+
+# Clean and wrangle
 metadata <- metadata[,-1]
-# sc_counts <- data.frame(column_to_rownames(sc_counts, var = "Row.names"))
-names(sc_counts)[names(sc_counts) == 'Row.names'] <- 'gene'
+names(sc_counts)[names(sc_counts) == 'Row.names'] <- 'external_gene_name'
+
 sc_counts <- sc_counts[,-1] 
 # take the rows with all 0s 
-sc_counts_clean <- sc_counts[apply(sc_counts[,-1], 1, function(x) !all(x == 0)), ]
+sc_counts <- sc_counts[apply(sc_counts[, -1], 1, function(x) !all(x == 0)), ]
+
+# get ensmbl gene ids
+library(biomaRt)
+
+#ensembl <- useMart("ensembl", host="uswest.ensembl.org", dataset="mmusculus_gene_ensembl", ensemblRedirect = FALSE)
+genes_names <- sc_counts$external_gene_name
+
+#IDs <- getBM(attributes=c('ensembl_gene_id',
+#                          'external_gene_name'),
+#             filters = 'mgi_symbol',
+#             values = genes_names,
+#             mart = ensembl)
+IDs <- read_csv("/Users/laurasansc/github/CoDA_scRNAseq/data/martquery_0530110047_39.txt.gz")
+names(IDs)[names(IDs) == 'Gene stable ID'] <- 'ensembl_gene_id'
+names(IDs)[names(IDs) == 'Gene name'] <- 'external_gene_name'
+
+
+# get annotated genes
+annotated_genes_counts <- merge(IDs,sc_counts,by="external_gene_name")
+annotated_genes_counts <- data.frame(column_to_rownames(annotated_genes_counts, var = "ensembl_gene_id"))
+annotated_genes_counts <- annotated_genes_counts[,-1] 
+genes <- row.names(annotated_genes_counts)
+
 
 # ------------------- start general visualitzations -------------------------
 # Wrangle for visualizations
-row.names(sc_counts_clean) <- sc_counts_clean$gene
-sc_counts_clean <- sc_counts_clean[, -1]
+#row.names(sc_counts_clean) <- sc_counts_clean$gene
+#sc_counts_clean <- sc_counts_clean[, -1]
 
 # heat map raw counts random sample
 set.seed(123539)
-cols <- sample(1:ncol(sc_counts_clean), 50)
-rows <- sample(1:nrow(sc_counts_clean), 50)
+cols <- sample(1:ncol(annotated_genes_counts), 50)
+rows <- sample(1:nrow(annotated_genes_counts), 50)
 
 # First pick random cols and rows
-sc_counts_clean <- sc_counts_clean.sort_index()
-random_raw_counts <- sc_counts[rows, cols]
+annotated_genes_counts <- annotated_genes_counts.sort_index()
+random_raw_counts <- annotated_genes_counts[rows, cols]
 
 # We need a long dataframe to plot the heatmap
 long_random_raw_counts <- random_raw_counts %>%
@@ -54,8 +78,8 @@ dev.off()
 
 # what if we do the same after DESEQ normalization and EDGER
 # open the files
-deseq_norm <- read_csv("/Users/laurasansc/Desktop/BioinformaticsSystemsBiology/CoDA_special_course/SC_deseq2_DE.csv.gz")
-edge_norm <- read_csv("/Users/laurasansc/Desktop/BioinformaticsSystemsBiology/CoDA_special_course/SC_edgeR_DE.csv.gz")
+deseq_norm <- read_csv("/home/projects/cge/people/laucom/CoDA_scRNAseq/results/SC/SC_deseq2_DE_new.csv.gz")
+edge_norm <- read_csv("/home/projects/cge/people/laucom/CoDA_scRNAseq/results/SC/SC_edgeR_DE.csv.gz")
 
 
 # get top 500 and write to file
@@ -69,9 +93,9 @@ dds_norm <- dds_norm %>% arrange(X)
 edge_norm <- edge_norm %>% arrange(X)
 
 # clean data
-row.names(dds_norm) <- dds_norm$X
+row.names(dds_norm) <- dds_norm$X1
 dds_norm <- dds_norm[, -1]
-row.names(edge_norm) <- edge_norm$X
+row.names(edge_norm) <- edge_norm$X1
 edge_norm <- edge_norm[, -1]
 
 dds_norm_counts <- dds_norm[, -c(1, 2, 3, 4, 5, 6)]
@@ -133,15 +157,15 @@ dev.off()
 # --------- now with ALDEx2 ------------
 # what if we do the same after DESEQ normalization and EDGER
 # open the files
-aldex_norm <- read.csv(unz("results/bulk/aldex2_clr.csv.zip", "aldex2_clr.csv"))
+aldex_norm <- read_csv("/home/projects/cge/people/laucom/CoDA_scRNAseq/results/SC/aldex2_clr.csv.gz")
 
 # do the same plot as before:
 # sort the same
-aldex_norm <- aldex_norm %>% arrange(X)
+aldex_norm <- aldex_norm %>% arrange(X1)
 
 
 # clean data
-row.names(aldex_norm) <- aldex_norm$X
+row.names(aldex_norm) <- aldex_norm$X1
 aldex_norm <- aldex_norm[, -1]
 
 
@@ -181,13 +205,13 @@ dev.off()
 
 
 require(gridExtra)
-png("foo_counts.png", height = 2100, width = 2100)
+png("heatmap_1.png", height = 2100, width = 2100)
 grid.arrange(p1, p2, p3, p4, ncol = 2, nrow=2)
 dev.off()
 
 
 require("egg")
-png("foo_counts_2.png", height = 2100, width = 2100)
+png("heatmap_2.png", height = 2100, width = 2100)
 ggarrange(p1, p2, p3, p4, ncol = 2, nrow=2)
 dev.off()
 
